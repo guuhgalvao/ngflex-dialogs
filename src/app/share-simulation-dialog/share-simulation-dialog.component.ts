3f61af7b69f4d6bc6a2d08b43258f5e1ebae3205
guuhgalvao <gu.afgalvao@gmail.com>
2020-07-01 22:13:51 -0300

    share whatsapp

--- src/app/share-simulation-dialog/share-simulation-dialog.component.ts
+++ src/app/share-simulation-dialog/share-simulation-dialog.component.ts
@@ -23,4 +23,11 @@ export class ShareSimulationDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 
+  shareZapZap(): void {
+    let msg = `Marca: Chevrolet\nAno do modelo: 2018\nAno de fabricação: 2018\n\n*48x de R$ 208,33*`;
+    let target = `https://api.whatsapp.com/send?phone=${encodeURIComponent('+55 11 99451-7370')}&text=${encodeURIComponent(msg)}`;
+    window.open(target);
+    // console.log('test');
+  }
+
 }
